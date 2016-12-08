d77e231f8716367460253cf826a7b4c002f0fb86
Eduardo Lujan <eduardo.lujan.p@gmail.com>
2016-12-08 10:17:07 -0600

    Added: Changes to instal icu4 version

--- update_tzdata.sh
+++ update_tzdata.sh
@@ -5,18 +5,19 @@
 # CHANGE THIS: 
 # go to https://www.iana.org/time-zones and get the URL for the last DATA
 # release
-TZDATAVERSION="tzdata2016j.tar.gz"
-URLTZDATASRC="https://www.iana.org/time-zones/repository/releases/${TZDATAVERSION}"
+
+TZDATAVERSION="tzdata2016j"
+TZDATAFILE="${TZDATAVERSION}.tar.gz"
+URLTZDATASRC="https://www.iana.org/time-zones/repository/releases/${TZDATAFILE}"
 TZDATADIR="/tmp/tzdata"
-### update tzdata
-cd /tmp
+# Update tzdata
 if [ -d "${TZDATADIR}" ]; then
   rm -rf "${TZDATADIR}"
 fi
 mkdir "${TZDATADIR}"
 cd "${TZDATADIR}"
 wget "${URLTZDATASRC}"
-tar -xzf "${TZDATAVERSION}"
+tar -xvzf "${TZDATAFILE}"
 sudo zic africa
 sudo zic antarctica
 sudo zic asia
@@ -29,4 +30,30 @@ sudo zic northamerica
 sudo zic pacificnew
 sudo zic southamerica
 sudo zic systemv
+echo "${TZDATAVERSION} installed"
+echo "Done ..."
+
+# Install icu4version
+ICUVERSION=`ls /usr/share/icu/*.dat | grep -o "\(\d\d\)l\.dat" | grep -o "^\d\d"`
+URLICUSRC="http://download.icu-project.org/files/icu4c/${ICUVERSION}.1/icu4c-${ICUVERSION}_1-src.zip"
+ICUDIR="/tmp/icu4c"
+cd "${ICUDIR}"
+if [ -d "${ICUDIR}" ]; then
+  rm -rf "${ICUDIR}"
+fi
+mkdir "${ICUDIR}"
+cd "${ICUDIR}"
+wget "${URLICUSRC}" -O icusrc.tar.gz
+tar -xvzf icusrc.tar.gz
+cd icu/source/tools/tzcode
+# Download the same version of tzdata
+wget "${URLTZDATASRC}"
+cd ../../
+# Compile
+./runConfigureICU MacOSX --with-data-packaging=archive
+gnumake
+sudo install -o root -g wheel -m 0644 -Sp "data/out/icudt${ICUVERSION}l.dat" "/usr/share/icu/icudt${ICUVERSION}l.dat"
+echo "${ICUVERSION} installed"
+echo "Done ..."
+
 
